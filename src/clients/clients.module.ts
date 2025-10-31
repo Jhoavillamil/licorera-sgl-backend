9d1076ce235158e858452b1f54ecfa4d3517e2af
Jhoan Vera <jhoan.vera2509@hotmail.com>
2025-10-30 22:05:04 -0500

    chore: crear rama main con README

--- src/clients/clients.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Cliente } from '../entities/cliente/cliente.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Cliente])],
-  exports: [TypeOrmModule],
-})
-export class ClientsModule {}
--- src/suppliers/suppliers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Supplier } from '../entities/supplier/supplier.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Supplier])],
-  exports: [TypeOrmModule],
-})
-export class SuppliersModule {}
